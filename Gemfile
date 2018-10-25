e8c24e7928779266627ac0ff0dbc8922b30bd160
Rui Freitas <dinnerpartysaboteur@gmail.com>
2018-10-25 13:33:38 +0100

    Initial commit with devise template from https://github.com/lewagon/rails-templates

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+ruby '2.4.3'
+
+gem 'bootsnap', require: false
+gem 'devise'
+gem 'jbuilder', '~> 2.0'
+gem 'pg', '~> 0.21'
+gem 'puma'
+gem 'rails', '5.2.1'
+gem 'redis'
+
+gem 'autoprefixer-rails'
+gem 'bootstrap-sass', '~> 3.3'
+gem 'font-awesome-sass', '~> 5.0.9'
+gem 'sass-rails'
+gem 'simple_form'
+gem 'uglifier'
+gem 'webpacker'
+
+group :development do
+  gem 'web-console', '>= 3.3.0'
+end
+
+group :development, :test do
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'listen', '~> 3.0.5'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'dotenv-rails'
+end
---
+++ config/application.rb
@@ -0,0 +1,24 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Devhire
+  class Application < Rails::Application
+    config.generators do |generate|
+          generate.assets false
+          generate.helper false
+          generate.test_framework  :test_unit, fixture: false
+        end
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
