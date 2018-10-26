6854b0cda13f252773e5b0e74fe6b510f268146e
Rui Freitas <dinnerpartysaboteur@gmail.com>
2018-10-26 23:01:48 +0100

    User can select dates with datepicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,16 @@
 import 'bootstrap';
+import './application.css';
 import { toggleIcons } from '../components/bookmarking';
+import { toggleDateInputs } from '../components/booking';
 
 const homePage = document.querySelector('.pages.home');
 if (homePage) {
   toggleIcons();
 }
 
-const devIndex = document.querySelector('.developers.index');
-if (devIndex) {
+const devIndexPage = document.querySelector('.developers.index');
+if (devIndexPage) {
   toggleIcons();
 }
+
+toggleDateInputs();
